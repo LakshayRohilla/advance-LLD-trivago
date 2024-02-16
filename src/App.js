6f4b774a1fa01108419d94d822f5c309ef92fd91
LakshayRohilla10 <lakshay.rohilla@sms-group.com>
2024-02-16 18:55:12 +0530

    rest of the page component and few other fixes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import Header from './component/header';
 import HeaderBody from './component/header-body';
 import CompleteSeachBar from './component/complete-search-bar';
 import { Container } from '@mui/material';
+import RestOfPage from './component/rest-of-page';
 
 function App() {
   return (
-    <Container>
-      <Header/>
-      <HeaderBody/>
-      <CompleteSeachBar/>
-    </Container>
+    <>
+      <div style={{backgroundColor:'#F5F5F5'}}>
+        <Container >
+          <Header/>
+          <HeaderBody/>
+          <CompleteSeachBar/>
+        </Container>
+      </div>
+      <Container>
+        <RestOfPage/>
+      </Container>
+    </>
   );
 }
 
--- src/component/header-body.jsx
+++ src/component/header-body.jsx
@@ -1,15 +1,16 @@
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { Container } from '@mui/material';
 
 export default function HeaderBody(){
     return (
-        <Box sx={{ marginLeft: 4, marginTop: 9 }}>
-        <Typography variant="h5" sx={{ flexGrow: 1 , fontWeight: 600}}>
+        <Container sx={{ marginLeft: 2, marginTop: 9 }}>
+        <Typography variant="h5" sx={{fontWeight: 600}}>
         We compare hotel prices from 100s of sites
           </Typography>
           <Typography >
           We’ll do the searching. You do the saving.
         </Typography>
-        </Box>
+        </Container>
     );
 }
