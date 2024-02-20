d7b9df4eabd11667bb733a976cb5bede78eba712
LakshayRohilla10 <lakshay.rohilla@sms-group.com>
2024-02-20 18:22:22 +0530

    adding functionality in the location section of the search bar

--- src/component/location-component.jsx
+++ src/component/location-component.jsx
@@ -1,22 +1,53 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import Stack from '@mui/material/Stack';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 
 export default function Location() {
+  
+  // const top100Films = [
+  //   { title: 'Jaipur' },
+  //   { title: 'Delhi' },
+  //   { title: 'Mumbai'},
+  //   { title: 'Kolkata'},
+  //   { title: 'Chennai'},
+  // ];
+
+  const cities = ["Jaipur", "Delhi", "Mumbai", "Kolkata", 'Chennai'];
+  const [selectedCity, setSelectedCity] = useState();
+  // const [cityInputValue, setCityInputValue] = useState("");
+  useEffect(()=>{
+    console.log(selectedCity);
+  }, [selectedCity]);
+  // console.log(selectedCity);
+  
+
   return (
     <Stack sx={{mt: -2}}>
       <Autocomplete 
+      // ----------------------------------
+      value={selectedCity}
+      onChange={(event, newCity) => {
+        setSelectedCity(newCity);
+      }}
+      // inputValue={cityInputValue}
+      // onInputChange={(event, newCityInputValue) => {
+      //   setCityInputValue(newCityInputValue);
+      // }}
+      // --------------------------------------
         id="size-small-standard"
         size="small"
-        options={top100Films}
-        getOptionLabel={(option) => option.title}
+        options={cities}
+        // --------------------------------------In case we have the provided array as a object "START"--------------------------
+        // options={top100Films}
+        // getOptionLabel={(option) => option.title}
+        // --------------------------------------In case we have the provided array as a object "END"--------------------------
         renderInput={(params) => (
           <TextField 
             {...params}
             variant="standard"
             label="Where to ?"
-            placeholder="Favorites"
             InputProps={{...params.InputProps, disableUnderline: true}} 
           />
         )}
@@ -25,12 +56,6 @@ export default function Location() {
   );
 }
 
-const top100Films = [
-  { title: 'Jaipur' },
-  { title: 'Delhi' },
-  { title: 'Mumbai'},
-  { title: 'Kolkata'},
-  { title: 'Chennai'},
-];
+
 
 
